77eec47fae62e3ef8410ed48f1dff5cbf034329c
Kelvin Chua <chuakid@gmail.com>
2021-07-13 20:04:54 +0800

    cors

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const controller = require('./controller');
+const cors = require('cors')
 const port = 8000;
 const app = express();
+require('dotenv').config()
 
+if (process.env.NODE_ENV === "development") {
+    app.use(cors())
+}
 app.use('/api', controller);
 
 app.listen(port, () => { console.log("Listening on port " + port); })
